dae0045c61c57a17dc9be65a82e5a77e5eedef12
Thomas Recouvreux <thomas.recouvreux@nanxia.fr>
2025-11-27 11:32:19 +0100

    Remove pypdf2

--- docia/tests/data/processor/extraction_text_from_attachments/test_text_extract_pdf.py
+++ docia/tests/data/processor/extraction_text_from_attachments/test_text_extract_pdf.py
@@ -7,7 +7,7 @@ def test_extract_text_from_pdf():
     with open(ASSETS_DIR / "lettre.pdf", "rb") as f:
         file_content = f.read()
 
-    text, is_ocr = extract_text_from_pdf(file_content, "file.pdf")
+    text, is_ocr = extract_text_from_pdf(file_content)
     assert not is_ocr
     assert_similar_text(text, 0.999)
 
@@ -16,6 +16,6 @@ def test_extract_text_from_pdf_ocr():
     with open(ASSETS_DIR / "lettre-ocr.pdf", "rb") as f:
         file_content = f.read()
 
-    text, is_ocr = extract_text_from_pdf(file_content, "file.pdf")
+    text, is_ocr = extract_text_from_pdf(file_content)
     assert is_ocr
     assert_similar_text(text, 0.95)
