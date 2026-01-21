b07ef6aacbcf02785310afcde2ae5aea07c844de
Foulques <fouques.renard@gmail.com>
2026-01-21 15:41:55 +0100

    Lancement de la branche

---
+++ tests_e2e/test_quality_classification.py
@@ -0,0 +1,11 @@
+# create_batch_test 
+# Trouve le csv (pareil que dans les autres tests)
+# Crée le dataframe
+# Récupère uniquement les lignes avec une certaine classification dans le GT
+# Passe les tests dans la classification et récupère un tableau des résultats
+# Renvoier les df : df_test, df_result, df_merged
+
+# Ajoute une focntion de comparaison pour la classification
+
+# Ajouter une fonction montrant les résultats pour globaux et lignes par ligne
+
