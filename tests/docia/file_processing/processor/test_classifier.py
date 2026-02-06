7a0e8f3d22643bb726503d98649f7133c24eb5ea
Foulques <fouques.renard@gmail.com>
2026-02-06 12:31:01 +0100

    modif test

--- tests/docia/file_processing/processor/test_classifier.py
+++ tests/docia/file_processing/processor/test_classifier.py
@@ -5,6 +5,6 @@ from docia.file_processing.processor.classifier import DIC_CLASS_FILE_BY_NAME, c
 
 def test_classify_file_with_llm():
     with patch("docia.file_processing.processor.analyze_content.LLMClient.ask_llm", autospec=True) as m:
-        m.return_value = "Extrait Kbis"
+        m.return_value = ["Extrait Kbis"]
         r = classify_file_with_llm("doc.pdf", "Hello World", DIC_CLASS_FILE_BY_NAME)
     assert r == "kbis"
